1754fc2161890ea8b9d2e58ba5a3b0da7061d438
Vinayaka V Nayak <106983430+vinayak-v-nayak@users.noreply.github.com>
2024-11-20 16:19:15 +0530

    Delete accounts directory

--- accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import register, login_view, dashboard, transfer_funds
-
-urlpatterns = [
-    path('register/', register, name='register'),
-    path('login/', login_view, name='login'),
-    path('dashboard/', dashboard, name='dashboard'),
-    path('transfer/', transfer_funds, name='transfer_funds'),
-]
